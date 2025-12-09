e4898f5047e48a4ba733001d9607b2cec769698a
Your Name <johndifelice@protonmail.com>
2025-12-08 19:55:14 -0500

    deleted dist

--- backend/dist/services/runDocxExtractor.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// --- Run Example ---
-const docxExtractor_1 = require("./docxExtractor");
-const main = async () => {
-    const result = await (0, docxExtractor_1.extractCommentsWithTargets)("C:\\Projects\\forwriters\\backend\\src\\services\\1764259138560-Balk-Feedback.docx");
-    return result;
-};
-main();
