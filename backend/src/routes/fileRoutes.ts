5883f4c63c15c2975b29c2043b2b73671470b157
Your Name <johndifelice@protonmail.com>
2025-12-11 13:48:58 -0500

    added multerS3.AUTO_CONTENT_TYPE so S3 files will be stored as application/pdf

--- backend/src/routes/fileRoutes.ts
+++ backend/src/routes/fileRoutes.ts
@@ -20,6 +20,7 @@ const upload = multer({
   storage: multerS3({
     s3,
     bucket: process.env.AWS_S3_BUCKET!,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (_req, file, cb) => {
       const filename = `${Date.now()}-${file.originalname}`;
       cb(null, filename);
